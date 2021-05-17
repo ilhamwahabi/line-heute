160c6e6f7cf5148f7fc19b8d56b4ceee4384bb29
iwgx <wahabiputra@gmail.com>
2021-05-17 10:55:19 +0700

    Init swr

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,18 @@
 import React from "react";
+import useSWR from "swr";
+
+const fetcher = (input: RequestInfo, init: RequestInit | undefined) =>
+  fetch(input, init).then((res) => res.json());
 
 function App() {
+  const { data, error } = useSWR(
+    "https://jsonp.afeld.me/?url=https://today.line.me/id/portaljson",
+    fetcher
+  );
+
+  if (error) return <div>failed to load</div>;
+  if (!data) return <div>loading...</div>;
+
   return (
     <div className="bg-blue-500 text-white min-h-screen w-full flex items-center justify-center text-5xl">
       LINE Heute
